c4639a1edf7138758a6576cce8ef407c213022a2
mak3042001 <mak3042001@gmail.com>
2023-04-02 16:00:09 +0200

    new entity for details commit

---
+++ lib/movies/domain/entities/movies_details.dart
@@ -0,0 +1,37 @@
+import 'package:equatable/equatable.dart';
+import 'package:movies_app/movies/domain/entities/genres.dart';
+
+class MoviesDetails extends Equatable {
+  final List<Genres> genres;
+  final String backdropPath;
+  final String releaseDate;
+  final String overview;
+  final String title;
+  final int id;
+  final int runtime;
+  final double voteAverage;
+
+  const MoviesDetails({
+    required this.genres,
+    required this.title,
+    required this.releaseDate,
+    required this.runtime,
+    required this.backdropPath,
+    required this.overview,
+    required this.id,
+    required this.voteAverage,
+  });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+    backdropPath,
+    overview,
+    id,
+    voteAverage,
+    releaseDate,
+    runtime,
+    title,
+    genres,
+  ];
+}
